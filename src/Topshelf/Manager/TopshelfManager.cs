f086a68523f5ed28b18fb6820b62803e61c7a215
nrjohnstone <locotewz@gmail.com>
2017-03-02 23:28:16 +0100

    move the responsibility for processing installer settings into a process argument builder
    into a separate class

--- src/Topshelf/Manager/TopshelfManager.cs
+++ src/Topshelf/Manager/TopshelfManager.cs
@@ -80,81 +80,7 @@ namespace Cake.Topshelf
                     _Log.Warning("Process timed out!");
                 }
             }
-
-            private ProcessArgumentBuilder GetInstallArguments(TopshelfSettings settings)
-            {
-                ProcessArgumentBuilder builder = new ProcessArgumentBuilder();
-
-                if ((settings != null) && (settings.Arguments != null))
-                {
-                    builder = settings.Arguments;
-                }
-
-                builder.Append(new TextArgument("install"));
-
-                if (settings != null)
-                {
-                    if (!string.IsNullOrWhiteSpace(settings.Username))
-                    {
-                        builder.Append(new TextArgument("-username"));
-                        builder.Append(new QuotedArgument(new TextArgument(settings.Username)));
-                    }
-
-                    if (!string.IsNullOrWhiteSpace(settings.Password))
-                    {
-                        builder.Append(new TextArgument("-password"));
-                        builder.Append(new QuotedArgument(new TextArgument(settings.Password)));
-                    }
-
-                    if (!string.IsNullOrWhiteSpace(settings.Instance))
-                    {
-                        builder.Append(new TextArgument("-instance"));
-                        builder.Append(new QuotedArgument(new TextArgument(settings.Instance)));
-                    }
-
-                    builder.Append(settings.Autostart
-                            ? new TextArgument("--autostart")
-                            : new TextArgument("--manual"));
-
-                    if (settings.Disabled)
-                        builder.Append(new TextArgument("--disabled"));
-
-                    if (settings.Delayed)
-                        builder.Append(new TextArgument("--delayed"));
-
-                    if (settings.LocalSystem)
-                        builder.Append(new TextArgument("--localsystem"));
-
-                    if (settings.LocalService)
-                        builder.Append(new TextArgument("--localservice"));
-
-                    if (settings.NetworkService)
-                        builder.Append(new TextArgument("--networkservice"));
-
-                    if (!string.IsNullOrWhiteSpace(settings.ServiceName))
-                    {
-                        builder.Append(new TextArgument("--servicename"));
-                        builder.Append(new QuotedArgument(new TextArgument(settings.ServiceName)));
-                    }
-
-                    if (!string.IsNullOrWhiteSpace(settings.Description))
-                    {
-                        builder.Append(new TextArgument("--description"));
-                        builder.Append(new QuotedArgument(new TextArgument(settings.Description)));
-                    }
-
-                    if (!string.IsNullOrWhiteSpace(settings.DisplayName))
-                    {
-                        builder.Append(new TextArgument("--displayname"));
-                        builder.Append(new QuotedArgument(new TextArgument(settings.DisplayName)));
-                    }
-                }
-
-                return builder;
-            }
-
-
-
+        
             /// <summary>
             /// Installs a Topshelf windows service
             /// </summary>
@@ -173,7 +99,7 @@ namespace Cake.Topshelf
                     timeout = settings.Timeout;
                 }
 
-                this.ExecuteProcess(filePath, this.GetInstallArguments(settings), timeout);
+                this.ExecuteProcess(filePath, InstallArgumentsFactory.Create(settings), timeout);
 
                 _Log.Verbose("Topshelf service installed.");
             }
