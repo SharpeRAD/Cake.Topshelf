750bf58c92f75c945d3d36189eb59088de056c71
nrjohnstone <locotewz@gmail.com>
2017-03-02 23:24:04 +0100

    cover remaining logic around installer settings with tests

--- src/Topshelf.Tests/Tests/Manager/TopshelfManagerTests.cs
+++ src/Topshelf.Tests/Tests/Manager/TopshelfManagerTests.cs
@@ -1,6 +1,9 @@
-﻿using Cake.Core.IO;
+﻿using System.Configuration;
+using Cake.Core.IO;
+using FluentAssertions;
 using NSubstitute;
 using Xunit;
+using Xunit.Sdk;
 
 namespace Cake.Topshelf.Tests.Manager
 {
@@ -9,6 +12,7 @@ namespace Cake.Topshelf.Tests.Manager
         private readonly DebugLog _debugLog;
         private readonly IProcessRunner _processRunner;
         private readonly IProcess _process;
+        private ProcessSettings _processSettingsPassed;
 
         const int ExpectedDefaultTimeoutMs = 60000;
 
@@ -19,7 +23,10 @@ namespace Cake.Topshelf.Tests.Manager
             _process = Substitute.For<IProcess>();
 
             _processRunner.Start(Arg.Any<FilePath>(), Arg.Any<ProcessSettings>())
-                .Returns(_process);
+               .Returns(_process).AndDoes(callinfo =>
+               {
+                   _processSettingsPassed = callinfo.Arg<ProcessSettings>();
+               });
         }
 
         private TopshelfManager CreateSut()
@@ -52,6 +59,195 @@ namespace Cake.Topshelf.Tests.Manager
                 .WaitForExit(100);
         }
 
+        [Fact]
+        public void InstallService_ShouldAddInstallToTopshelfArgs()
+        {
+            var sut = CreateSut();
+      
+            sut.InstallService(new FilePath("SomePath"));
+
+            _processSettingsPassed.Arguments.Render().Should().Be("install");
+        }
+
+        [Fact]
+        public void InstallService_WhenSetting_AutoStart_ShouldAddToTopshelfArgs()
+        {
+            var sut = CreateSut();
+
+            sut.InstallService(new FilePath("SomePath"), new TopshelfSettings()
+            {
+                Autostart = true
+            });
+
+            _processSettingsPassed.Arguments.Render().Should().Be("install --autostart");
+        }
+
+        [Fact]
+        public void InstallService_WhenNOtSetting_AutoStart_ShouldAddManualToTopshelfArgs()
+        {
+            var sut = CreateSut();
+
+            sut.InstallService(new FilePath("SomePath"), new TopshelfSettings()
+            {
+                Autostart = false
+            });
+
+            _processSettingsPassed.Arguments.Render().Should().Be("install --manual");
+        }
+
+        [Fact]
+        public void InstallService_WhenSetting_Delayed_ShouldAddToTopshelfArgs()
+        {
+            var sut = CreateSut();
+
+            sut.InstallService(new FilePath("SomePath"), new TopshelfSettings()
+            {
+                Delayed = true
+            });
+
+            _processSettingsPassed.Arguments.Render().Should().StartWith("install ");
+            _processSettingsPassed.Arguments.Render().Should().Contain(" --delayed");
+        }
+
+        [Fact]
+        public void InstallService_WhenSetting_UserName_ShouldAddToTopshelfArgs()
+        {
+            var sut = CreateSut();
+
+            sut.InstallService(new FilePath("SomePath"), new TopshelfSettings()
+            {
+                Username = "Nathan"
+            });
+
+            _processSettingsPassed.Arguments.Render().Should().StartWith("install ");
+            _processSettingsPassed.Arguments.Render().Should().Contain(" -username \"Nathan\" ");
+        }
+
+        [Fact]
+        public void InstallService_WhenSetting_Password_ShouldAddToTopshelfArgs()
+        {
+            var sut = CreateSut();
+
+            sut.InstallService(new FilePath("SomePath"), new TopshelfSettings()
+            {
+                Password = "foo"
+            });
+
+            _processSettingsPassed.Arguments.Render().Should().StartWith("install ");
+            _processSettingsPassed.Arguments.Render().Should().Contain(" -password \"foo\" ");
+        }
+
+        [Fact]
+        public void InstallService_WhenSetting_Instance_ShouldAddToTopshelfArgs()
+        {
+            var sut = CreateSut();
+
+            sut.InstallService(new FilePath("SomePath"), new TopshelfSettings()
+            {
+                Instance = "bar"
+            });
+
+            _processSettingsPassed.Arguments.Render().Should().StartWith("install ");
+            _processSettingsPassed.Arguments.Render().Should().Contain(" -instance \"bar\" ");
+        }
+
+        [Fact]
+        public void InstallService_WhenSetting_Disabled_ShouldAddToTopshelfArgs()
+        {
+            var sut = CreateSut();
+
+            sut.InstallService(new FilePath("SomePath"), new TopshelfSettings()
+            {
+                Disabled = true
+            });
+
+            _processSettingsPassed.Arguments.Render().Should().StartWith("install ");
+            _processSettingsPassed.Arguments.Render().Should().Contain(" --disabled");
+        }
+
+        [Fact]
+        public void InstallService_WhenSetting_LocalSystem_ShouldAddToTopshelfArgs()
+        {
+            var sut = CreateSut();
+
+            sut.InstallService(new FilePath("SomePath"), new TopshelfSettings()
+            {
+                LocalSystem = true
+            });
+
+            _processSettingsPassed.Arguments.Render().Should().StartWith("install ");
+            _processSettingsPassed.Arguments.Render().Should().Contain(" --localsystem");
+        }
+
+        [Fact]
+        public void InstallService_WhenSetting_LocalService_ShouldAddToTopshelfArgs()
+        {
+            var sut = CreateSut();
+
+            sut.InstallService(new FilePath("SomePath"), new TopshelfSettings()
+            {
+                LocalService = true
+            });
+
+            _processSettingsPassed.Arguments.Render().Should().StartWith("install ");
+            _processSettingsPassed.Arguments.Render().Should().Contain(" --localservice");
+        }
+
+        [Fact]
+        public void InstallService_WhenSetting_NetworkService_ShouldAddToTopshelfArgs()
+        {
+            var sut = CreateSut();
+
+            sut.InstallService(new FilePath("SomePath"), new TopshelfSettings()
+            {
+                NetworkService = true
+            });
+
+            _processSettingsPassed.Arguments.Render().Should().StartWith("install ");
+            _processSettingsPassed.Arguments.Render().Should().Contain(" --networkservice");
+        }
+
+        [Fact(Skip = "ExistingBug!")]
+        public void InstallService_WhenSetting_ServiceName_ShouldAddToTopshelfArgs()
+        {
+            var sut = CreateSut();
+
+            sut.InstallService(new FilePath("SomePath"), new TopshelfSettings()
+            {
+                ServiceName = "Terminator"
+            });
+
+            _processSettingsPassed.Arguments.Render().Should().StartWith("install ");
+            _processSettingsPassed.Arguments.Render().Should().Contain(" --servicename \"Terminator\"");
+        }
+
+        [Fact]
+        public void InstallService_WhenSetting_Description_ShouldAddToTopshelfArgs()
+        {
+            var sut = CreateSut();
+
+            sut.InstallService(new FilePath("SomePath"), new TopshelfSettings()
+            {
+                Description = "SomeDescription"
+            });
+
+            _processSettingsPassed.Arguments.Render().Should().StartWith("install ");
+            _processSettingsPassed.Arguments.Render().Should().Contain(" --description \"SomeDescription\"");
+        }
+
+        [Fact]
+        public void InstallService_WhenSetting_DisplayName_ShouldAddToTopshelfArgs()
+        {
+            var sut = CreateSut();
+
+            sut.InstallService(new FilePath("SomePath"), new TopshelfSettings()
+            {
+                DisplayName = "SomeDisplayName"
+            });
+
+            _processSettingsPassed.Arguments.Render().Should().StartWith("install ");
+            _processSettingsPassed.Arguments.Render().Should().Contain(" --displayname \"SomeDisplayName\"");
+        }
         [Fact]
         public void UninstallService_WhenNoSettingsSupplied_ShouldUseDefaultTimeout()
         {
